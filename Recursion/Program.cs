269f94a722f2b37e2c8346e12bce189ad15a7975
RemziBalakishiyev <remzibalakishiyev13@gmail.com>
2024-07-30 12:25:27 +0400

    Add project files.

---
+++ Recursion/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+//int n = 3;
+
+////Fun1(n);
+////Console.WriteLine(Fun2(5)) ;
+//int x = 0;
+//Console.WriteLine(Fun3(5)) ;
+//void Fun1(int n)
+//{
+//    if (n > 0)
+//    {
+//        Fun1(n - 1);
+//        Console.WriteLine(n);
+//    }
+//}
+
+
+//int Fun2(int n)
+//{
+//    if (n>0)
+//    {
+//        return Fun2(n - 1) + n;
+//    }
+//    return 0;
+//}
+
+
+//int  Fun3(int n)
+//{
+//    if (n > 0)
+//    {
+//        x++;
+//        return Fun3(n - 1) + x;
+//    }
+//    return 0;
+//}
+
+
+using Recursion;
+
+RecursionPractice recursionPractice = new RecursionPractice();
+recursionPractice.Build();
---
+++ Recursion/RecursionPractice.cs
@@ -0,0 +1,26 @@
+﻿namespace Recursion
+{
+    public class RecursionPractice
+    {
+        public void Build()
+        {
+            Console.WriteLine(Power(2,5));
+        }
+
+        public int Power(int m, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else if (n % 2 == 0)
+            {
+                return Power(m * m, n / 2);
+            }
+            else
+            {
+                return m * Power(m * m, (n - 1) / 2);
+            }
+        }
+    }
+}
